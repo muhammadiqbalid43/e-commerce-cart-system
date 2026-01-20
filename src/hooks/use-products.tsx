61c96a831cc0e3bcfe26f6f7fa4535e3454b9c1f
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2026-01-20 09:48:18 +0700

    feat: add error

--- src/hooks/use-products.tsx
+++ src/hooks/use-products.tsx
@@ -1,9 +1,13 @@
 import { getProducts } from "@/lib/api/products";
 import { useQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export function useProducts() {
   return useQuery({
     queryKey: ["products"],
     queryFn: getProducts,
+    meta: {
+      errorMessage: toast("Failed to load products"),
+    },
   });
 }
